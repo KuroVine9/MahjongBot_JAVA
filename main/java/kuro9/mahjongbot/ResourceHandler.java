23d035dc8f76a0900c78bfa368d80fd7fc5adc78
KuroVine9 <kurovine9@gmail.com>
2023-01-25 23:10:28 +0900

    리소스번들 에러 수정

--- main/java/kuro9/mahjongbot/ResourceHandler.java
+++ main/java/kuro9/mahjongbot/ResourceHandler.java
@@ -3,21 +3,28 @@ package kuro9.mahjongbot;
 import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import net.dv8tion.jda.api.interactions.DiscordLocale;
 
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ResourceHandler {
+
+    final static ArrayList<DiscordLocale> supportLocale = new ArrayList<>(2) {{
+        add(DiscordLocale.KOREAN);
+        add(DiscordLocale.JAPANESE);
+    }};
+
     public static ResourceBundle getResource(GenericInteractionCreateEvent event) {
         return ResourceBundle.getBundle(
                 "localization/Instructions",
-                Locale.forLanguageTag(event.getUserLocale().getLocale())
+                supportLocale.contains(event.getUserLocale()) ? Locale.forLanguageTag(event.getUserLocale().getLocale()) : Locale.ROOT
         );
     }
 
     public static ResourceBundle getResource(DiscordLocale locale) {
         return ResourceBundle.getBundle(
                 "localization/Instructions",
-                Locale.forLanguageTag(locale.getLocale())
+                supportLocale.contains(locale) ? Locale.forLanguageTag(locale.getLocale()) : Locale.ROOT
         );
     }
 }
